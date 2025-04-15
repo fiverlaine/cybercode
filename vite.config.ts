4020eb2c5a217460883158bd1adfc64bd6057638
fiverlaine <fiverlaine123@gmail.com>
2025-04-15 16:26:16 -0300

    Atualização completa do projeto: removidas pastas e arquivos não utilizados

--- vite.config.ts
+++ vite.config.ts
@@ -7,18 +7,4 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  server: {
-    proxy: {
-      '/proxy': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/archive': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
 });
